937e1fb8f84c94bc4376f16c9cf1139899165996
LesPaul997 <mariogiordano941@gmail.com>
2024-05-27 20:47:31 +0200

    add: function

--- script/main.js
+++ script/main.js
@@ -1,45 +1,109 @@
 'use strict';
 
 //Elementi della Pagina
-const formCalculator = document.getElementById('calculator-form');
+const form = document.querySelector('form');
 const resultElement = document.getElementById('result');
 const userName = document.getElementById('firstName');
 const userSurname = document.getElementById('lastName');
 const userEmail = document.getElementById('email');
-const userCountry = document.getElementById('country');
+const warningCodeText = document.getElementById('warningCode');
+const userWork = document.getElementById('selectWork');
+const codeImput = document.getElementById('code');
+const warningCheckText = document.getElementById('warningCheck');
+const priceText = document.getElementById('price');
 
 //Elementi della traccia
-const workerBackend = 20.50;
-const workerFrontend = 15.30;
-const workerAnalysis = 33.60;
-const variableTime = 60;
-const promotionalCode = document.getElementsById('formGroupExampleInput');
-const workHours = 10;
+
+//Codici sconto
+const code = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+let promotionalCode = "";
+
+const jobHours = 10;
+const discount = 25;
+
+let userCode = "";
+let selectedJob = "";
+let hourPrice = 0;
+let fullPrice = 0;
+let intPrice = 0;
+let decimalPrice = 0;
+
+//Qui elenco le tre selezioni 
+const userJob = {
+    "Sviluppo Backend": 20.50,
+    "Sviluppo Frontend": 15.30,
+    "Analisi Progettuale": 33.60
+};
+
+addOptions(userJob);
+
+
+
 
 // COSE DA FARE ALL'INVIO DEL FORM
-formCalculator.addEventListener('submit', function (event) {
+form.AddEventListener("submit", function (event) {
     // Impedisco il  ricaricamento della pagina
     event.preventDefault();
 
-    // Raccolgo i dati nel form
-    const country = userCountry.value;
-    const promo = parseInt(promotionalCode.value);
+    warningCodeText.classList.add("d-none");
+    warningCheckText.classList.add('d-none');
 
-    //VALIDATION 
-    // Se qualcosa non va, non proseguo
-    if (isNaN(country)) {
-        resultElement.innerText = 'sono stati inseriti dati non validi';
-            resultElement.classList.add('text-danger');
-            return;
+     //Controllo check privacy
+     if (checkElement.checked) {
+        //se la property checked è true, allora si richiama la funzione di calcolo
+        calculatePrice();
     }
-
-     // Calcolo il prezzo base
-     let preventiveBackend = workerBackend * variableTime;
-     let preventiveFrontend = workerFrontend * variableTime;
-     let preventiveAnalysis = workerAnalysis * variableTime;
-
-
-     //PROGETTO IN STANDBY
-     //CONTINUARE COL CALCOLO E LA STAMPA RILEGGENDO BENE I CAMPI SCRITTI
+    else {
+        warningCheckText.classList.remove('d-none')
+    };
 
 });
+
+function calculatePrice() {
+    //Si estrae il valore dalla select
+    selectedJob = userWork.value;
+
+    hourPrice = userJob[selectedJob];
+
+    //Calcolo del prezzo finale
+    fullPrice = hourPrice * jobHours;
+
+    //Viene letto il codice se inserito
+    promotionalCode = codeImput.value;
+
+    //Verifichiamo se il codice inserito è valido
+    if (userCode !== "") {
+
+        if (code.includes(userCode)){
+
+            //Prezzo scontato
+            fullPrice -= fullPrice * (discount / 100);
+
+            warningCodeText.classList.remove("text-danger");
+            warningCodeText.classList.add("text-success");
+            warningCodeText.innerText = "Il codice inserito è valido";
+            warningCodeText.classList.remove("d-none");
+
+        }   else {
+            warningCodeText.classList.remove("text-success");
+            warningCodeText.classList.add("text-danger");
+            warningCodeText.innerText = "Il codice inserito non è valido";
+            warningCodeText.classList.remove("d-none");
+        };
+    };
+    //Prezzo intero
+    intPrice = parseInt(fullPrice);
+
+    //Inserimento del prezzo intero all'interno del dom
+    intSpan.innerText = `€ ${intPrice}`;
+    decimalSpan.innerText = `,${decimalPrice}`;
+
+    //A questo punto il prezzo diventa visibile
+    priceText.classList.remove('d-none');
+};
+
+function extractDecimal(num, n) {
+    const decimal = ((num - parseInt(num)).toFixed(n)).toString();
+    //viene restituita solo la parte decimale
+    return decimal.split(".")[1];
+};
